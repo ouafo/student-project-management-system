d3f538a30d87a1ff712ef2c26b74441ee5f63037
ouafo <cybeleouafo@yahoo.com>
2026-05-25 17:23:30 +0200

    fix in merge

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
@@ -21,7 +21,7 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
@@ -44,4 +44,4 @@ def main():
 
 if __name__ == '__main__':
     main()
->>>>>>> Passy_branch
+
